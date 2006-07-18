62d5b528b1604c289ba77f4c888e9cd05d0198f1
Hartmut Seichter <hartmut@gmail.com>
2006-07-18 07:22:16 +0000

    added VX example
    git-svn-id: file:///Users/hartmut/OldCode/uARToolKit/trunk@32 d5e7133f-6a0c-0410-a427-901a366c8858

---
+++ example/uSimpleVX/usimplevx.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+/* include from VX */
+#include <vx.h>
+
+/* include from ARToolkit */
+#include <artoolkit.h>
+
+
+/* a state variable */
+arToolkit *art = 0;
+
+int initialised = 0;
+
+/* info about the image */
+int width, height;
+
+/* pointer to markers */
+artMarker *hiro_marker, *kanji_marker;
+
+
+
+int 
+vxCaptureCallback(vxSource* src, vxFrame* frame, void *userdata)
+{
+
+	if (!initialised) {
+
+		/* configure the size we are actually using */
+		artParamChangeSize(art,frame->info.width,frame->info.height);
+		initialised++;
+
+	}
+
+	artSetImage(art, frame->image.data, 
+		frame->info.width, 
+		frame->info.height, ART_PIXFORMAT_RGB);
+
+	/* detect the marker */
+	artDetect(art);
+
+	/* generate info for the markers */
+	artUpdateMarker(art,ART_BASIC);
+
+	/* now the time has come to see what happened */
+	if (hiro_marker->visible)
+	{
+		printf("Hiro!\n");
+	};
+
+	return vxNext;
+}
+
+
+
+int main(int argc, char* argv[])
+{
+
+	vxModule* module;
+	vxSource* source;
+
+
+	/* initialise a new AR Toolkit state */
+	art = artCreate();
+
+	/* set debugging */
+	artSetInteger(art, artDebug, 0);
+
+	/* load a pattern onto a marker */
+	hiro_marker = artLoadMarker(art,"Data/patt.hiro",80.0);
+
+
+	/* load the camera parameter */
+	artLoadCameraParameter(art,"Data/camera_para.dat");
+
+
+	/** register a module */
+	module = vxLoadModule(argv[1]);
+
+	/** create a source from that module */
+	source = vxCreate(module);
+
+	/** configure the source */
+	vxConfig(source, "v4lsrc ! ffmpegcolorspace ! capsfilter caps=video/x-raw-rgb,bpp=24 ! identity name=vxsink ! ffmpegcolorspace ! autovideosink");
+
+	/** start capturing */
+	vxStart(source,&vxCaptureCallback,0);
+	
+
+	while (vxOK == vxUpdate(source,vxNext)) {
+
+	};
+
+
+}
